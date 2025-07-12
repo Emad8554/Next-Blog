cfbf458f515acfbefee986782825f939f3175081
Emad Ahmed <emad264645@gmail.com>
2025-07-12 05:40:27 +0300

    fixed mongodb

--- src/lib/action/user.js
+++ src/lib/action/user.js
@@ -13,28 +13,29 @@ export const createOrUpdateUser = async (
   try {
     await connect();
     const user = await User.findOneAndUpdate(
-        {clerkId: id},
-        {
-            $set: {
-                firstName: first_name,
-                lastName: last_name,
-                profilePicture: image_url,
-                email: email_addresses[0].email_address,
-                username,
-            },
-        }, {new: true, upsert: true}
-    )
+      { clerkId: id },
+      {
+        $set: {
+          firstName: first_name,
+          lastName: last_name,
+          profilePicture: image_url,
+          email: email_addresses[0].email_address,
+          username,
+        },
+      },
+      { new: true, upsert: true }
+    );
     return user;
   } catch (error) {
-    console.log('Error creating or updating user:', error);
+    console.log("Error creating or updating user:", error);
   }
 };
 
 export const deleteUser = async (id) => {
-    try {
-        await connect();
-        await User.findOneAndDelete({clerkId: id});
-    } catch(error) {
-        console.log('Error deleting user: ', error);
-    }
-}
+  try {
+    await connect();
+    await User.findOneAndDelete({ clerkId: id });
+  } catch (error) {
+    console.log("Error deleting user:", error);
+  }
+};
